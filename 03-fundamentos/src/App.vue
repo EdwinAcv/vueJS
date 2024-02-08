dc7615dc48526cc218f5e1a2666d499220e9e42a
Edwin <edwin.correo.g@gmail.com>
2024-02-08 10:01:21 -0400

    Fin de la seccion 4

---
+++ 03-fundamentos/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <counter :start = "15"/>
+  </div>
+</template>
+
+<script lang="ts">
+import Counter from './components/Counter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Counter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 03-fundamentos/src/components/Counter.vue
@@ -0,0 +1,69 @@
+
+<template>
+    <div>
+        <div>
+            <h2 >{{ tittle || customTittle }}</h2>
+            <p> {{ counter }} <sup>2</sup> = {{ squereCounter }} </p>
+        </div>
+
+        <div>
+            <button @:click='increase'>+1</button>
+            <button @:click='decrease'>-1</button>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+
+    props: {
+        tittle: String,
+        start: {
+            type: Number,
+            default: 10
+        }
+    }  ,
+    // name: 'Patito'
+    data(){
+        return {
+            counter: this.start
+        }
+    },
+
+    methods: {
+        increase(){
+            this.counter ++
+        },
+        decrease(){
+            this.counter --
+        }
+    },
+    computed: {
+        squereCounter(){
+            return this.counter * this.counter;
+        },
+        customTittle(){
+            return 'Counter';
+        }
+    }
+}
+</script>
+
+<style>
+    button {
+        background-color: #64b687;
+        border-radius: 5px;
+        border: 1px solid white;
+        color: white;
+        cursor: pointer;
+        margin: 0 5px;
+        padding: 5px 15px;
+        transition: 0.3s ease-in-out;
+    }
+
+    button:hover {
+        background-color: #5aa67b;
+        transition: 0.3s ease-in-out;
+    }
+</style>
